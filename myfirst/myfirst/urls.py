19f75fb56707d7037eefbe308ed55a399dcde75f
Yuliya Potemkina <potemkina-92@list.ru>
2022-12-21 14:39:59 +0300

    new

--- myfirst/myfirst/urls.py
+++ myfirst/myfirst/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from myfirstapp.views import index
+from myfirst import settings
+from myfirstapp.views import index, pageNotFound
+from myfirstapp.views import categories
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myfirstapp/', index)
+    path('', include('myfirstapp.urls'))
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
--- myfirst/myfirstapp/views.py
+++ myfirst/myfirstapp/views.py
@@ -1,6 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseNotFound, Http404
+
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return HttpResponse('Страница приложения MyfirstApp')
+
+def categories(request, catid):
+    if request.GET:
+        print(request.GET)
+
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}<p>')
+
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=True
+                        )
+
+
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound(f'<h1>Страница не найдена</h1>')
